e34a0661865c3e95cb3bd1237f4a05312026e63e
H-Vining <hv49@cornell.edu>
2014-06-02 03:51:54 -0400

    Some Changes

--- RAD3input.m
+++ RAD3input.m
@@ -1,4 +1,4 @@
-function RAD3input(xmax,f,mumax,kd,xfirststage, RA, D3, RAmin, D3min,td)
+function test10(xmax,f,mumax,kd,xfirststage, RA, D3, RAmin, D3min)
 %Shows the cell density versus time for a given cell with these parameters
 % xmax = maximum cell density, f = self renewal probability, mumax =
 % maximal growth rate, kd = death rate, xfirststage = initial cell 
@@ -7,18 +7,20 @@ function RAD3input(xmax,f,mumax,kd,xfirststage, RA, D3, RAmin, D3min,td)
 %reach final stage
 initcon = [];
 initval = [];
-for i = 1:4
+for i = 1:5
     initcon = [initcon, 1e-4];
     if i == 1
         initval = [initval,xfirststage];
     elseif i~= 1 && i<3
         initval = [initval,0];
-    else
+    elseif i <5
         initval = [initval,0];
+    else
+        initval = [initval,mumax*(1-sum(initval(1:4))/xmax)];
     end
 end
 options = odeset('RelTol',1e-4, 'AbsTol',initcon);
-[T, X] = ode45(@(t,x) RAD3model1(t,x,xmax,f,mumax,kd,RA,D3,RAmin,D3min,td), [0,145], initval,options);
+[T, X] = ode45(@(t,x) test11(t,x,xmax,f,mumax,kd,RA,D3,RAmin,D3min), [0,145], initval,options);
 hold on
 XR = X(:,1);
 X2 = X(:, 2);
@@ -27,10 +29,10 @@ X4 = X(:, 4);
 %for j = 2:(4)
  %   XR(:,1) = XR(:,1)+X(:,j);
 %nd
-plot(T*mumax, XR(:,1)/xmax, 'b');
-plot(T*mumax, X3(:,1)/xmax, 'r');
-plot(T*mumax, X2(:,1)/xmax, 'g');
-plot(T*mumax, X4(:,1)/xmax, 'm');
+plot(T*mumax, XR(:,1), 'b');
+plot(T*mumax, X3(:,1), 'r');
+plot(T*mumax, X2(:,1), 'g');
+plot(T*mumax, X4(:,1), 'm');
 xlabel('Tau, dimensionless time')
 ylabel('xsum*, dimensionless cell density of stages')
 title(['Time versus Cell Density for Self-Renewal Rate of', num2str(f)])
